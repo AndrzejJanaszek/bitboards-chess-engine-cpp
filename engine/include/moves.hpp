5cfc59509ddc9f69514da13cc5730168e696e90e
Andrzej Janaszek <janaszek.andrzej.szkola@gmail.com>
2025-11-14 01:15:25 +0100

    feat: get best move first version - simple evaluation no beta alpha pruning

--- engine/include/moves.hpp
+++ engine/include/moves.hpp
@@ -42,6 +42,7 @@ void make_move(Move move, Board &board);
 
 std::vector<Move> generate_legal_moves(Board &game_state);
 
-bool isKingUnderAttack(Board &board);
+// bool isKingUnderAttack(Board &board);
+bool isKingUnderAttack(Board &board, bool other_side = false);
 
 bool isCheckMate(Board &board);
